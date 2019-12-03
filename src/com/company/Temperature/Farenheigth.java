dd9d73d780afe92cf0a64b374171044370e7720b
Khikar-1997 <57430976+Khikar-1997@users.noreply.github.com>
2019-12-03 06:51:47 -0800

    Delete Farenheigth.java

--- src/com/company/Temperature/Farenheigth.java
+++
@@ -1,8 +0,0 @@
-package com.company.Temperature;
-
-public class Farenheigth extends AbstractTemperatureUnit {
-    @Override
-    public void printConverter() {
-        System.out.println(getTemperature() + " F");
-    }
-}

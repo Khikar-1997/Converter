25975c0ca4bb6b78ceca9649754da0ce7fab4fe6
Khikar-1997 <57430976+Khikar-1997@users.noreply.github.com>
2019-12-03 06:51:55 -0800

    Delete Kelvin.java

--- src/com/company/Temperature/Kelvin.java
+++
@@ -1,8 +0,0 @@
-package com.company.Temperature;
-
-public class Kelvin extends AbstractTemperatureUnit {
-    @Override
-    public void printConverter() {
-        System.out.println(getTemperature() + " K");
-    }
-}

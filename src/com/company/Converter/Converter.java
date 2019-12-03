8aa2ad49ba0a724d80c959844b572c1c57042631
Khikar-1997 <57430976+Khikar-1997@users.noreply.github.com>
2019-12-03 06:52:28 -0800

    Delete Converter.java

--- src/com/company/Converter/Converter.java
+++
@@ -1,7 +0,0 @@
-package com.company.Converter;
-
-import com.company.Temperature.AbstractTemperatureUnit;
-
-public interface Converter {
-    AbstractTemperatureUnit convert(AbstractTemperatureUnit unit);
-}

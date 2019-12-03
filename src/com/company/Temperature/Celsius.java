ef395bf104338d2b4156b85122181861d9b51065
Khikar-1997 <57430976+Khikar-1997@users.noreply.github.com>
2019-12-03 06:51:40 -0800

    Delete Celsius.java

--- src/com/company/Temperature/Celsius.java
+++
@@ -1,8 +0,0 @@
-package com.company.Temperature;
-
-public class Celsius extends AbstractTemperatureUnit implements PrintConverter {
-    @Override
-    public void printConverter() {
-        System.out.println(getTemperature() + " °C");
-    }
-}
